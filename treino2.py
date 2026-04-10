ffa1648fd0ac8cb6624305ed7c805ef6d49203b5
Lucas Silva de Abreu <lucas.sabreu2008@gmail.com>
2026-04-10 10:34:16 -0300

    Update treino2.py

--- treino2.py
+++ treino2.py
@@ -1,6 +1,6 @@
 nome = (input("Digite o seu nome: "))
 serie = (input("Digite a sua sala: "))
-nota = int(input("Digite sua nota (de 0 a 100): "))
+nota = float(input("Digite sua nota (de 0 a 100): "))
 
 if nota>=60:
     print("Parabéns", nome + ",", "você foi aprovado!")
